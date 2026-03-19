a792901d954fb5e2702bf20e43767bde1c59dd27
SMK321-myprojects <smktalasu15@gmail.com>
2026-03-19 17:18:12 +0530

    added comment for invalid credentials

--- fraud/src/main/java/com/customer/exception/AdminInvalidCredentials.java
+++ fraud/src/main/java/com/customer/exception/AdminInvalidCredentials.java
@@ -1,5 +1,6 @@
 package com.customer.exception;
 
+/*For invalid credentials*/
 public class AdminInvalidCredentials extends RuntimeException {
    public AdminInvalidCredentials(String message)
    {
